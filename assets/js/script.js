62c91f2e7c19d15f2cbd5bd6c502705ca29660b3
Daniela <dani.revecod@gmail.com>
2020-03-05 21:08:56 -0400

    orden de codigo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-$(function(){
+$(function() {
 
-  $("a").click(function(event){
+  $("a").click(function(event) {
     if (this.hash !== "") {
       event.preventDefault();
 
@@ -8,12 +8,14 @@ $(function(){
 
       $("html, body").animate({
         scrollTop: $(gato).offset().top
-      }, 800, function(){
+      }, 800, function() {
         window.location.hash = gato;
       });
     }
   });
-  window.onload=function(){alert('Bienvenido al Portafolio de Magdalena');}
+  window.onload = function() {
+    alert('Bienvenido al Portafolio de Magdalena');
+  }
 
 
 });
